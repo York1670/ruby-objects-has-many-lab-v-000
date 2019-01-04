70d2e2544ae35e28effa274d3895427ff78cfb3a
Bobbi York <york1670@gmail.com>
2019-01-04 05:51:23 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -5,10 +5,6 @@ class Post
   def initialize(title)
     @title = title
     @@all << title
-  end #end of initialize
-
-  def self.allxx
-    @@all
-  end #end all
+  end #end of initialize 
 
 end #end of class Post
